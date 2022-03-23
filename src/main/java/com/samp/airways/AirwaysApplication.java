3f0189b811e5a70fe12f2a5abfbd7fc0ba02dc81
mrinmay678 <mrinmay678@gmail.com>
2022-03-23 12:49:31 +0530

    first commit

---
+++ src/main/java/com/samp/airways/AirwaysApplication.java
@@ -0,0 +1,13 @@
+package com.samp.airways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samp/airways/AirwaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samp.airways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
